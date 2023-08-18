edf5f819349bd5dca367ec83a858e803146f2574
Jeffrey Scott <valenpendragon@gmail.com>
2023-08-18 15:58:28 -0400

    Conform classes/__init__.py to PEP standards. Add entities/display_models.py to store models ConfigTableModel and for displaying the tables used by this application. Add entities/__init__.py to expose the display models classes for direct import.

---
+++ entities/__init__.py
@@ -0,0 +1,2 @@
+from .display_models import CombatActionTableModel, ConfigTableModel, \
+    CombatTargetTableModel
---
+++ entities/display_models.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import Qt
+import pandas as pd
+
+
+class ConfigTableModel(QtCore.QAbstractTableModel):
+    pass
+
+
+class CombatActionTableModel(QtCore.QAbstractTableModel):
+    pass
+
+
+class CombatTargetTableModel(QtCore.QAbstractTableModel):
+    pass
