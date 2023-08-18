057c9654a3afbbb55eaae853c1e53a4984581952
Jeffrey Scott <valenpendragon@gmail.com>
2023-08-18 13:55:18 -0400

    Add new file config_windows.py to store classes related to ConfigurationWindow.

---
+++ classes/config_windows.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QFileDialog, QMessageBox, \
+    QApplication, QMainWindow, QStatusBar, QLabel, QHBoxLayout
+import sys
+import os
